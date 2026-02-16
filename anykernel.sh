9606ce3536c7ded973a8914da5f2be1aa9a38b05
Dylan <liamsdk99@gmail.com>
2026-02-16 02:43:28 -0300

    spacewar: Initial

--- anykernel.sh
+++ anykernel.sh
@@ -1,14 +1,13 @@
 properties() { '
-kernel.string=Excalibur
-kernel.revision=4.19
+kernel.string=Specatravel
+kernel.revision=5.4
 kernel.made=@LiamSDK
 anykernel3.made=osm0sis @ xda-developers
-kernel.compiler=WeebX Clang
-message.word=Thank you for installing Excalibur!
+kernel.compiler=ZyC clang
+message.word=Thank you for installing Specatravel!
 do.devicecheck=1
 do.cleanup=1
-device.name1=sea
-device.name2=ocean
+device.name1=spacewar
 supported.versions=12.0-16.0
 supported.patchlevels=
 supported.vendorpatchlevels=
